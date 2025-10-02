d95dcec2c46457e407f029cbcfd1ff83d256409f
cgalvis <cgalvis@humboldt.org.co>
2025-10-02 10:37:34 -0500

    refactor: added some changes in mkdocs paths

--- docs/main.py
+++ docs/main.py
@@ -2,12 +2,12 @@
 def define_env(env):
     """
     Called by mkdocs-macros-plugin at build time.
-    We load docs/_data/cli.yml and expose it as {{ cli }} in templates.
+    We load docs/data/cli.yml and expose it as {{ cli }} in templates.
     """
     import os, yaml
 
-    # Ruta absoluta al YAML
-    yml_path = os.path.join(env.project_dir, "docs", "_data", "cli.yml")
+    # YAML relative path
+    yml_path = os.path.join(env.project_dir, "docs", "data", "cli.yml")
 
     with open(yml_path, "r", encoding="utf-8") as f:
         env.variables["cli"] = yaml.safe_load(f)
