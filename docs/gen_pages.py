3ec54f0bf55d97480bda52813b23a9f24cd24a71
cgalvis <cgalvis@humboldt.org.co>
2025-10-03 09:52:01 -0500

    docs: enabled dynamic nav content generation

--- docs/gen_pages.py
+++ docs/gen_pages.py
@@ -5,6 +5,8 @@ template_path = os.path.join("docs", "command.md")
 
 commands_dir = os.path.join("docs", "commands")
 
+summary_lines = ["- [Home](index.md)\n", "- Commands\n"]
+
 def gen_files():
     """
     Generate md files from yml files.
@@ -15,8 +17,14 @@ def gen_files():
     for fname in sorted(os.listdir(commands_dir)):
         if fname.endswith(".yml"):
             name = os.path.splitext(fname)[0]
+            md_file = f"{name}.md"
 
-            with mkdocs_gen_files.open(f"{name}.md", "w") as f:
+            with mkdocs_gen_files.open(md_file, "w") as f:
                 f.write(template_content)
+            
+            summary_lines.append(f"    - [{name.capitalize()}]({md_file})\n")
+
+    with mkdocs_gen_files.open("summary.md", "w") as f:
+        f.writelines(summary_lines)
 
 gen_files()
--- docs/summary.md
+++
@@ -1,6 +0,0 @@
-* [Home](index.md)
-* Commands examples
-    * [Create](create.md)
-    * [Inject](inject.md)
-    * [Remove](remove.md)
-    * [Validate](validate.md)
